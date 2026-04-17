0a584a00727a0222e503561a92272243ea427c14
LMeira0 <lmeira@unifesp.br>
2026-04-16 22:07:35 -0300

    Update 1013.C

--- 1013.C
+++ 1013.C
@@ -1,7 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Leonardo Freire Meira
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1013
 Exercício   : 1013
 Data        : 07/04/2026
 Objetivo    : Descobrir qual eh o maior valor.
