0f68b6129dc8336a455dc6980a35cd76e09c44dc
LMeira0 <lmeira@unifesp.br>
2026-04-16 22:08:10 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,7 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Leonardo Freire Meira
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1000
 Exercício   : 1000
 Data        : 07/04/2026
 Objetivo    : Mostrar hello world.
