1b870d4d0cd6e9e3cfb150a14098b1369abcfae6
LMeira0 <lmeira@unifesp.br>
2026-05-19 20:59:00 -0300

    Create 1046.C

---
+++ 1046.C
@@ -0,0 +1,35 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Leonardo Freire Meira
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1000
+Exercício   : 1000
+Data        : 07/04/2026
+Objetivo    : Calcular a duracao do jogo.
+Aprendizado : .
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int calcularDuracao(int inicio, int fim){
+
+    if (inicio < fim) {
+        return fim - inicio;
+    }
+    else {
+        return 24 - inicio + fim;
+    };
+}
+
+int main() {
+
+    int a, b, c;
+
+    scanf("%d %d", &a, &b);
+
+    c = calcularDuracao(a, b);
+
+    printf("O JOGO DUROU %d HORA(S)", c);
+
+    return 0;
+}
