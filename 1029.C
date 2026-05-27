d93abda00e7a90f7278781b36453817c938f15a3
LMeira0 <lmeira@unifesp.br>
2026-05-26 21:33:03 -0300

    Update 1029.C

--- 1029.C
+++ 1029.C
@@ -1,1 +1,40 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Leonardo Freire Meira
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1028
+Data        : 26/05/2026
+Objetivo    : calcular o fibonacci de algum numero.
+Aprendizado : calcular o fibonacci usando recursao.
+-------------------------------------------------------------------------- */
 
+#include <stdio.h>
+
+int fibo(int a, int *p){
+    (*p)++;
+    if(a==0) {
+        return 0;
+    }
+    if(a==1) {
+        return 1;
+    }
+
+    return fibo(a - 1, p) + fibo(a - 2, p);
+}
+int main() {
+
+    int i, n, x, f, j = 0;
+
+    scanf("%d", &n);
+
+    for(i=0; i < n; i++){
+        j = 0;
+        scanf("%d", &x);
+
+        f = fibo(x, &j);
+
+        printf("fib(%d) = %d calls = %d\n", x, j - 1, f);
+    }
+
+    return 0;
+}
