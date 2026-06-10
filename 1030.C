06bf2abbd5fec27ebdf3007ba60ae43f6e0cd847
LMeira0 <lmeira@unifesp.br>
2026-06-09 23:39:14 -0300

    Update 1030.C

--- 1030.C
+++ 1030.C
@@ -1,35 +1,50 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Leonardo Freire Meira
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1030
+Exercício   : 1030
+Data        : 09/06/2026
+Objetivo    : Saber quem que vai sobra na roda.
+Aprendizado : .
+-------------------------------------------------------------------------- */
 #include <stdio.h>
- 
+
 int main() {
- 
-    int nc, n, k, i, f, j;
+    int nc, n, k, i, f, j, count;
     scanf("%d", &nc);
 
-    for(i=0; i < nc; i++){
-        scanf("%d", &n);
-        scanf("%d", &k);
+    for(i = 0; i < nc; i++) {
+        scanf("%d %d", &n, &k);
         int l[n];
-        for(f=0;f < n; f++){
+        
+        for(f = 0; f < n; f++) {
             l[f] = f + 1;
         }
-        f=0;
-        j=0;
-        while(j < n - 1){
-            
-            if(l[f] == k){
-                f = k+f;
-                l[f] = 0;
-            }
-            if(l[f] == 0){
-                f++;
-                continue;
-            }
-            if(l[f] != 0){
-                l[f] = 0;
+
+        f = 0; 
+        for (j = 0; j < n - 1; j++) {
+            count = 0; 
+            while (count < k) {
+                if (l[f] != 0) { 
+                    count++; 
+                }
+                
+                if (count == k) {
+                    l[f] = 0; 
+                    break;
+                }
+                f = (f + 1) % n;
             }
         }
-        printf("Case %d: %d", i+1, );
+
+        for(f = 0; f < n; f++) {
+            if(l[f] != 0) {
+                printf("Case %d: %d\n", i + 1, l[f]);
+                break;
+            }
+        }
     }
- 
+
     return 0;
 }
