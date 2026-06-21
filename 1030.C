2e0860a699ae20cc07e77bf6fee4785a8560923b
LMeira0 <lmeira@unifesp.br>
2026-06-21 15:23:40 -0300

    Update 1030.C

--- 1030.C
+++ 1030.C
@@ -5,45 +5,26 @@ Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/problems/view/1030
 Exercício   : 1030
 Data        : 09/06/2026
-Objetivo    : Saber quem que vai sobra na roda.
+Objetivo    : Saber quem que vai sobrar na roda (Problema de Josephus).
 Aprendizado : .
 -------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
-    int nc, n, k, i, f, j, count;
+    int nc, n, k, i, j;
+    int sobrevivente;
+
     scanf("%d", &nc);
 
-    for(i = 0; i < nc; i++) {
+    for (i = 0; i < nc; i++) {
         scanf("%d %d", &n, &k);
-        int l[n];
+        sobrevivente = 0;
         
-        for(f = 0; f < n; f++) {
-            l[f] = f + 1;
+        for (j = 2; j <= n; j++) {
+            sobrevivente = (sobrevivente + k) % j;
         }
-
-        f = 0; 
-        for (j = 0; j < n - 1; j++) {
-            count = 0; 
-            while (count < k) {
-                if (l[f] != 0) { 
-                    count++; 
-                }
-                
-                if (count == k) {
-                    l[f] = 0; 
-                    break;
-                }
-                f = (f + 1) % n;
-            }
-        }
-
-        for(f = 0; f < n; f++) {
-            if(l[f] != 0) {
-                printf("Case %d: %d\n", i + 1, l[f]);
-                break;
-            }
-        }
+      
+        printf("Case %d: %d\n", i + 1, sobrevivente + 1);
     }
 
     return 0;
