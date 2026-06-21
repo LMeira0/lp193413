036700609b8f895d9aebf61bcfb2eabd09df25ab
LMeira0 <lmeira@unifesp.br>
2026-06-21 14:51:04 -0300

    Update 1234.C

--- 1234.C
+++ 1234.C
@@ -5,35 +5,38 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1234
 Data        : 26/04/2026
 Objetivo    : Intercalar entre maiuscula e minuscula a frase lida.
-Aprendizado : Usar fgets, usar extensoes para deixar maiuscula e minuscula.
+Aprendizado : Usar fgets, usar extensoes para deixar maiuscula e minuscula, e ler até EOF.
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 #include <ctype.h>
 
 int main() {
-
-    int i=0, maiscula = 1;
     char palavra[100];
 
-    fgets(palavra, 100, stdin);
+    while (fgets(palavra, 100, stdin) != NULL) {
+        
+        int i = 0;
+        int maiscula = 1; 
 
-    while (palavra[i] != '\0') {
-        if (palavra[i] == ' ' || palavra[i] == '\n') {
+        while (palavra[i] != '\0') {
+        
+            if (palavra[i] == ' ' || palavra[i] == '\n') {
+                i++;
+                continue;
+            }
+            if (maiscula == 1) {
+                palavra[i] = toupper(palavra[i]);
+            } 
+            else {
+                palavra[i] = tolower(palavra[i]);
+            }
+            
+            maiscula = !maiscula;
             i++;
-            continue;
         }
-        if (maiscula == 1) {
-            palavra[i] = toupper(palavra[i]);
-        }
-        else {
-            palavra[i] = tolower(palavra[i]);
-        }
-        maiscula = !maiscula;
-        i++;
+        printf("%s", palavra);
     }
-
-    printf("%s", palavra);
 
     return 0;
 }
