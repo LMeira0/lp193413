316461e7f8587003b1bde3b6d9847fb6bb434418
LMeira0 <lmeira@unifesp.br>
2026-06-21 14:56:25 -0300

    Update 1258.C

--- 1258.C
+++ 1258.C
@@ -21,8 +21,14 @@ typedef struct camiseta {
 int main() {
     int n, i, j, troca;
     camisa v[60], temp;
+    int primeiro_caso  = 1;
 
     while (scanf("%d", &n) && n != 0) {
+        
+        if (!primeiro_caso) {
+            printf("\n");
+        }
+        primeiro_caso = 0;
 
         for (i = 0; i < n; i++) {
             getchar();
